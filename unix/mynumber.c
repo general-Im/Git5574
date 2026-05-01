b2142ebf9039fbdf258a812de240b1f5f046a77f
imyuwan <dbdhks3136@gmail.com>
2026-05-01 17:10:27 +0900

    prime

--- unix/mynumber.c
+++ unix/mynumber.c
@@ -9,6 +9,22 @@ int main(){
         printf("%d",i);
         printf(" ");
     }
+    printf("\n");
+    for (int i = 2; i <= 100; i++) {
+        int is_prime = 1; // 일단 소수라고 가정
 
+        // 2부터 i의 직전 숫자까지 나누어보며 확인
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                is_prime = 0; // 나누어떨어지면 소수가 아님
+                break;
+            }
+        }
+
+        // 소수로 판별된 경우에만 출력
+        if (is_prime == 1) {
+            printf("%d ", i);
+        }
+    }
     return 0;
 }
